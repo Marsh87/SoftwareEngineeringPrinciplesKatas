44f0948ebd8ea18331ec7b6b7704faec3f483a77
CHILLI\Mahesh <Mahesh.Moodley@chillisoft.co.za>
2019-05-17 15:11:43 +0200

    comment: add comment for assistance

--- CharacterCopyKata_20190121/CharacterCopyKata_20190121/Copier.cs
+++ CharacterCopyKata_20190121/CharacterCopyKata_20190121/Copier.cs
@@ -18,6 +18,8 @@ namespace CharacterCopyKata_20190121
 
         public void Copy()
         {
+            // Unsure how refactor this method
+
             // TODO there are still 2 concepts in this code that are not fully encapsulated by the methods you refactored out, see below:
             var characterFromSource = GetCharacterFromSource(); // <== read
             while (CharacterIsNotNewLine(characterFromSource))
